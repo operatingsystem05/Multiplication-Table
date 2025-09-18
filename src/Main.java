55f51a92d8a60632f711d7fd10c1cf96f228f7d5
operatingsystem05 <notoumaima@gmail.com>
2025-09-18 15:23:08 +0200

    Finalized

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         Scanner scanner = new Scanner(System.in);  // Create Scanner object
 
         System.out.println("This is an app that gives you the multiplication table of any number");
