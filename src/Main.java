81e37cd2ef7a46be2e9f68368749074118d8f452
operatingsystem05 <notoumaima@gmail.com>
2025-09-18 15:05:09 +0200

    Finalized

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);  // Create Scanner object
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("This is an app that gives you the multiplication table of any number");
+        System.out.print("Enter a number: ");
+
+        int num = scanner.nextInt();  // Read user input
+
+        System.out.printf("Multiplication Table for %d :", num);
+
+        // Loop from 1 to 10 to print the multiplication table
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n", num, i, num * i);
+        }
+
+        scanner.close();
         }
     }
-}
